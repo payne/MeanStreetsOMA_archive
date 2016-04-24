948eea2b380ed5332a6916430d6a9fc62f4460ed
Matt Payne <Payne@MattPayne.org>
2016-04-24 17:16:08 -0500

    did not miss any tweets :-)

--- daily.sh
+++ daily.sh
@@ -3,4 +3,4 @@
 cd /Users/mpayne/MeanStreetsOMA_archive
 id=`tail -1 tweet.data  | awk ' { print $1;  }'`
 echo "last ID is $id"
-wc -l tweet.data;t timeline MeanStreetsOMA   -l -r -n 5000 -s $id   >> tweet.data ;wc -l tweet.data
+wc -l tweet.data;t timeline MeanStreetsOMA   -l -r -n 9000 -s $id   >> tweet.data ;wc -l tweet.data
