e7bcde9e13dc056ae2c4ca5e75dc97fa7e627b19
Matt Payne <Payne@MattPayne.org>
2018-07-15 20:14:06 -0500

    Following tutorial.

---
+++ organize1/README.md
@@ -0,0 +1,3 @@
+Trying to follow https://dzone.com/articles/how-to-use-twitter-api-using-nodejs
+as a start.
+
---
+++ organize1/search.js
@@ -0,0 +1,16 @@
+var Twit = require('twit');
+var config = require('./config');
+console.log(config);
+
+var T = new Twit(config);
+var params = {
+  q: 'akshay',
+	count: 100,
+} // this is the param variable which will have key and value 
+
+
+T.get('search/tweets', { q: 'banana since:2011-07-11', count: 100 }, function(err, data, response) {
+				  console.log(data)
+});
+
+
