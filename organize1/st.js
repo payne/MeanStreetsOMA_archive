77787e720499886e845ea4d934fe2a1ca8fdcf7f
Matt Payne <Payne@MattPayne.org>
2018-07-16 20:26:56 -0500

    st.js works for three tweets.

--- organize1/st.js
+++ organize1/st.js
@@ -4,7 +4,10 @@ var config = require('./config_twitter');
 var T = new Twitter(config);
 
 var idList=[ 889754214607572992  , 889760990853836800 , 889762329457377280 , 889770314187636736  ];
-T.get('statuses/lookup.json', {id: idList.join(",")}, function(err, data, response) {
+var ids='889754214607572992,889760990853836800,889762329457377280,889770314187636736';
+
+console.log(ids);
+T.get('statuses/lookup.json', {id: ids}, function(err, data, response) {
 				  console.log(JSON.stringify(data));
 });
 
